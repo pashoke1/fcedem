a625960c75581746689873223a8db04c09662950
pashoke1 <56249009+pashoke1@users.noreply.github.com>
2019-12-23 08:21:47 +0300

    lastcomm

--- edem.fc/admprin.php
+++ edem.fc/admprin.php
@@ -9,6 +9,7 @@ if ($_SESSION['admin'] == true) {
 	<link rel="stylesheet" type="text/css" href="style1.css">
 </head>
 <body>
+	<h1>Добавление игроков</h1>
 <?php
 require('bd.php');
 
@@ -34,8 +35,28 @@ while ($row = $result_set->fetch_assoc()) {
   <p>ID удалить:<input type="int" name="net" style="width: 2%;"></p>
   <input type="submit" name="">
 </form>
+<hr>
+<h1>Управление игроками</h1>
 <?php
-     
+     $query = "SELECT `id`,`name_pol`,`ban` FROM `users`";
+
+$result = mysqli_query($conn, $query);
+
+
+
+
+
+
+while ($row = mysqli_fetch_assoc($result)) {
+  print_r($row);
+  }
+  ?>
+  <form method="post">
+  <p>ID заблокировать:<input type="int" name="ban" style="width: 2%;"></p>
+  <p>ID разблокировать:<input type="int" name="razban" style="width: 2%;"></p>
+  <input type="submit" name="">
+</form>
+<?php
 
         if ($_POST['da'] != null) {
            $idv = $_POST['da'];
@@ -63,6 +84,17 @@ while ($row = $result_set->fetch_assoc()) {
            
         $query2 = "DELETE FROM `prin` WHERE `prin`.`id` = '$idn'";
         $result2 = mysqli_query($conn, $query2);
+        
+       }
+       if ($_POST['ban'] != null) {
+           $idb = $_POST['da'];
+           $queryb = "UPDATE `users` SET `ban` = 1 WHERE `users`.`id` = '$idb'";      
+       }
+       if ($_POST['net'] != null) {
+           $idr = $_POST['net'];
+           
+       $queryr = "UPDATE `users` SET `ban` = 0 WHERE `users`.`id` = '$idb'";
+        $resultr = mysqli_query($conn, $queryr);
         
        }
        ?>
